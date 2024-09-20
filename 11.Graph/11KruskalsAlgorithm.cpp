3e816eb3f351e0fa1e7582bdc87c351bcf5c6e61
Vishal Panwar <vishal4panwar@gmail.com>
2024-09-20 22:41:33 +0530

    add file

---
+++ 11.Graph/11KruskalsAlgorithm.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(vector<int> &a, vector<int> &b)
+{
+    return a[2] < b[2];
+}
+
+void makeSet(vector<int> &parent, vector<int> &rank, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+}
+
+int findParent(vector<int> &parent, int node)
+{
+    if (parent[node] == node)
+    {
+        return node;
+    }
+    return parent[node] = findParent(parent, parent[node]);
+}
+
+void unionSet(int u, int v, vector<int> &parent, vector<int> &rank)
+{
+    u = findParent(parent, u);
+    v = findParent(parent, v);
+
+    if (rank[u] < rank[v])
+    {
+        parent[u] = v;
+    }
+    else if (rank[v] < rank[u])
+    {
+        parent[v] = u;
+    }
+    else
+    {
+        parent[v] = u;
+        rank[u]++;
+    }
+}
+
+int minimumSpanningTree(vector<vector<int>> &edges, int n)
+{
+    int ans = 0;
+    vector<int> parent(n);
+    vector<int> rank(n);
+    makeSet(parent, rank, n);
+
+    sort(edges.begin(), edges.end(), cmp);
+
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = findParent(parent, edges[i][0]);
+        int v = findParent(parent, edges[i][1]);
+        int wt = edges[i][2];
+
+        if (u != v)
+        {
+            ans += wt;
+            unionSet(u, v, parent, rank);
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    return 0;
+}
